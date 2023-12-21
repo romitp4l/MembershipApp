f5e58c9be565004c8c51e24ca1e612f1061cfa6d
romitp4l <romitpal4@gmail.com>
2023-12-21 14:12:31 +0530

    twenty december

--- app/src/main/java/com/example/membershipapp/SplashActivity.java
+++ app/src/main/java/com/example/membershipapp/SplashActivity.java
@@ -1,40 +1,39 @@
 package com.example.membershipapp;
 
-import static android.os.Build.VERSION_CODES.R;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.telephony.TelephonyManager;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class SplashActivity extends AppCompatActivity {
     TextView textView;
+    Button floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        textView=findViewById(R.id.phone_number);
+        textView = findViewById(R.id.phone_number);
 
-
-
-       // String phoneNumber = SimInfoHelper.getPhoneNumber(this);
-
-        String phoneNumber = PhoneNumberUtils.getPhoneNumber(this);
+         String phoneNumber = SimInfoHelper.getPhoneNumber(this);
+       // String phoneNumber = PhoneNumberUtils.getPhoneNumber(this);
 
         textView.setText(phoneNumber);
         Toast.makeText(this, "Phone Number: " + phoneNumber, Toast.LENGTH_LONG).show();
 
+        floatingActionButton=findViewById(R.id.flotingButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
-
-
+    };
     }
-
-
-
-
 }
